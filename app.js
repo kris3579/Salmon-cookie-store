b36b4225e0b5629d733f9cc0b02605727e4eb5dd
Kris3579 <kristianesvelt@hotmail.com>
2018-07-16 12:58:00 -0700

    html skeleton, objects created, .gitignore added

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var firstAndPike =  {
+  minCustomers: '23',
+  maxCustomers: '65',
+  avgCookiesPerCustomer: '6.3',
+};
+
+var SeaTac = {
+  minCustomers: '3',
+  maxCustomers: '24',
+  avgCookiesPerCustomer: '1.2',
+};
+
+var SeaCenter = {
+  minCustomers: '11',
+  maxCustomers: '38',
+  avgCookiesPerCustomer: '3.7',
+};
+
+var CapHill = {
+  minCustomers: '20',
+  maxCustomers: '38',
+  avgCookiesPerCustomer: '2.3',
+};
+
+var Alki = {
+  minCustomers: '2',
+  maxCustomers: '16',
+  avgCookiesPerCustomer: '4.6',
+};

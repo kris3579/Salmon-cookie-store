a8fd7e79d195aabb06a40c50dc34876bc8f5771d
Kris3579 <kristianesvelt@hotmail.com>
2018-07-17 00:31:28 -0700

    finish CSS and index.html, added google font, js edits

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var firstAndPike =  {
   minCustomers: 23,
   maxCustomers: 65,
   avgCookiesPerCustomer: 6.3,
-  random: function() {
+  customersPerHour: function() {
     return Math.floor(Math.random()* ((this.maxCustomers-this.minCustomers)) + this.minCustomers) * this.avgCookiesPerCustomer;
   },
   cookiesPerHour: [],
@@ -17,7 +17,7 @@ var seaTac = {
   minCustomers: 3,
   maxCustomers: 24,
   avgCookiesPerCustomer: 1.2,
-  random: function() {
+  customersPerHour: function() {
     return Math.floor(Math.random()* ((this.maxCustomers-this.minCustomers)) + this.minCustomers) * this.avgCookiesPerCustomer;
   },
   cookiesPerHour: [],
@@ -28,7 +28,7 @@ var seaCenter = {
   minCustomers: 11,
   maxCustomers: 38,
   avgCookiesPerCustomer: 3.7,
-  random: function() {
+  customersPerHour: function() {
     return Math.floor(Math.random()* ((this.maxCustomers-this.minCustomers)) + this.minCustomers) * this.avgCookiesPerCustomer;
   },
   cookiesPerHour: [],
@@ -39,7 +39,7 @@ var capHill = {
   minCustomers: 20,
   maxCustomers: 38,
   avgCookiesPerCustomer: 2.3,
-  random: function() {
+  customersPerHour: function() {
     return Math.floor(Math.random()* ((this.maxCustomers-this.minCustomers)) + this.minCustomers) * this.avgCookiesPerCustomer;
   },
   cookiesPerHour: [],
@@ -50,7 +50,7 @@ var alki = {
   minCustomers: 2,
   maxCustomers: 16,
   avgCookiesPerCustomer: 4.6,
-  random: function() {
+  customersPerHour: function() {
     return (Math.floor(Math.random()* ((this.maxCustomers-this.minCustomers)) + this.minCustomers) * this.avgCookiesPerCustomer);
   },
   cookiesPerHour: [],
@@ -62,14 +62,14 @@ var seaTacTotal = seaTac.total;
 var seaCenterTotal = seaCenter.total;
 var capHillTotal = capHill.total;
 var alkiTotal = alki.total;
-// this loop uses a locations random funtion to generate a rounded, random number between the min and max customers
+// this loop uses a locations customersPerHour funtion to generate a rounded, random number between the min and max customers
 // that number is then pushed into the corrosponding locations cookiesPerHour array
 for (var i = 0; i < 15; i++) {
-  firstAndPike.cookiesPerHour.push(Math.round(firstAndPike.random(firstAndPike.minCustomers, firstAndPike.maxCustomers)));
-  seaTac.cookiesPerHour.push(Math.round(seaTac.random(seaTac.minCustomers, seaTac.maxCustomers)));
-  seaCenter.cookiesPerHour.push(Math.round(seaCenter.random(seaCenter.minCustomers, seaCenter.maxCustomers)));
-  capHill.cookiesPerHour.push(Math.round(capHill.random(capHill.minCustomers, capHill.maxCustomers)));
-  alki.cookiesPerHour.push(Math.round(alki.random(alki.minCustomers, alki.maxCustomers)));
+  firstAndPike.cookiesPerHour.push(Math.round(firstAndPike.customersPerHour(firstAndPike.minCustomers, firstAndPike.maxCustomers)));
+  seaTac.cookiesPerHour.push(Math.round(seaTac.customersPerHour(seaTac.minCustomers, seaTac.maxCustomers)));
+  seaCenter.cookiesPerHour.push(Math.round(seaCenter.customersPerHour(seaCenter.minCustomers, seaCenter.maxCustomers)));
+  capHill.cookiesPerHour.push(Math.round(capHill.customersPerHour(capHill.minCustomers, capHill.maxCustomers)));
+  alki.cookiesPerHour.push(Math.round(alki.customersPerHour(alki.minCustomers, alki.maxCustomers)));
 }
 // test arrays
 console.log(firstAndPike.cookiesPerHour);
@@ -81,49 +81,49 @@ console.log(alki.cookiesPerHour);
 for (var j = 0; j < storeHours.length; j++) {
   // Create an li for first and pike
   var liFirstAndPike = document.createElement('li');
-  liFirstAndPike.textContent = storeHours[j] + ' ' + firstAndPike.cookiesPerHour[j] + ' cookies';
+  liFirstAndPike.textContent = storeHours[j] + ' ' + firstAndPike.cookiesPerHour[j] + ' Cookies';
   document.getElementById('firstandpike').appendChild(liFirstAndPike);
   // Create an li for SeaTac Airport
   var liSeaTac = document.createElement('li');
-  liSeaTac.textContent = storeHours[j] + ' ' + seaTac.cookiesPerHour[j] + ' cookies';
+  liSeaTac.textContent = storeHours[j] + ' ' + seaTac.cookiesPerHour[j] + ' Cookies';
   document.getElementById('seatac').appendChild(liSeaTac);
   // Create an li for Seattle Center
   var liSeaCenter = document.createElement('li');
-  liSeaCenter.textContent = storeHours[j] + ' ' + seaCenter.cookiesPerHour[j] + ' cookies';
+  liSeaCenter.textContent = storeHours[j] + ' ' + seaCenter.cookiesPerHour[j] + ' Cookies';
   document.getElementById('seacenter').appendChild(liSeaCenter);
   // Create an li for Capitol Hill
   var liCapHill = document.createElement('li');
-  liCapHill.textContent = storeHours[j] + ' ' + capHill.cookiesPerHour[j] + ' cookies';
+  liCapHill.textContent = storeHours[j] + ' ' + capHill.cookiesPerHour[j] + ' Cookies';
   document.getElementById('caphill').appendChild(liCapHill);
   // Create an li for Alki
   var liAlki = document.createElement('li');
-  liAlki.textContent = storeHours[j] + ' ' + alki.cookiesPerHour[j] + ' cookies';
+  liAlki.textContent = storeHours[j] + ' ' + alki.cookiesPerHour[j] + ' Cookies';
   document.getElementById('alki').appendChild(liAlki);
   // These add the number of cookies together to a total number, one line for each location, to be used outside of the loop
-  firstAndPikeTotal = Math.round(firstAndPikeTotal += firstAndPike.random(firstAndPike.maxCustomers, firstAndPike.minCustomers));
-  seaTacTotal = Math.round(seaTacTotal += seaTac.random(seaTac.maxCustomers, seaTac.minCustomers));
-  seaCenterTotal = Math.round(seaCenterTotal += seaCenter.random(seaCenter.maxCustomers, seaCenter.minCustomers));
-  capHillTotal = Math.round(capHillTotal += capHill.random(capHill.maxCustomers, capHill.minCustomers));
-  alkiTotal = Math.round(alkiTotal += alki.random(alki.maxCustomers, alki.minCustomers));
+  firstAndPikeTotal = Math.round(firstAndPikeTotal += firstAndPike.customersPerHour(firstAndPike.maxCustomers, firstAndPike.minCustomers));
+  seaTacTotal = Math.round(seaTacTotal += seaTac.customersPerHour(seaTac.maxCustomers, seaTac.minCustomers));
+  seaCenterTotal = Math.round(seaCenterTotal += seaCenter.customersPerHour(seaCenter.maxCustomers, seaCenter.minCustomers));
+  capHillTotal = Math.round(capHillTotal += capHill.customersPerHour(capHill.maxCustomers, capHill.minCustomers));
+  alkiTotal = Math.round(alkiTotal += alki.customersPerHour(alki.maxCustomers, alki.minCustomers));
 }
 
 // uses total cookies found in loop to create an li for the total cookies at first and pike
 var liFirstAndPikeTotal = document.createElement('li');
-liFirstAndPikeTotal.textContent = 'Total: ' + firstAndPikeTotal + ' cookies';
+liFirstAndPikeTotal.textContent = 'Total: ' + firstAndPikeTotal + ' Cookies';
 document.getElementById('firstandpike').appendChild(liFirstAndPikeTotal);
 // uses total cookies found in loop to create an li for the total cookies at seatac airport
 var liSeaTacTotal = document.createElement('li');
-liSeaTacTotal.textContent = 'Total: ' + seaTacTotal + ' cookies';
+liSeaTacTotal.textContent = 'Total: ' + seaTacTotal + ' Cookies';
 document.getElementById('seatac').appendChild(liSeaTacTotal);
 // uses total cookies found in loop to create an li for the total cookies at seattle center
 var liSeaCenterTotal = document.createElement('li');
-liSeaCenterTotal.textContent = 'Total: ' + seaCenterTotal + ' cookies';
+liSeaCenterTotal.textContent = 'Total: ' + seaCenterTotal + ' Cookies';
 document.getElementById('seacenter').appendChild(liSeaCenterTotal);
 // uses total cookies found in loop to create an li for the total cookies captiol hill
 var liCapHillTotal = document.createElement('li');
-liCapHillTotal.textContent = 'Total: ' + capHillTotal + ' cookies';
+liCapHillTotal.textContent = 'Total: ' + capHillTotal + ' Cookies';
 document.getElementById('caphill').appendChild(liCapHillTotal);
 // uses total cookies found in loop to create an li for the total cookies at alki
 var liAlkiTotal = document.createElement('li');
-liAlkiTotal.textContent = 'Total: ' + alkiTotal + ' cookies';
+liAlkiTotal.textContent = 'Total: ' + alkiTotal + ' Cookies';
 document.getElementById('alki').appendChild(liAlkiTotal);
